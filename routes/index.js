65034366a74f111c115bcaa5a2479a81add1223a
WilTanaka <william.tanakinha@hotmail.com>
2023-06-15 16:49:21 -0300

    Inicio projeto

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home', { title: 'Express' });
 });
-
+router.get('/cliente', function(req, res, next) {
+  res.render('cliente', { title: 'Express' });
+});
+router.get('/contato', function(req, res, next) {
+  res.render('contato', { title: 'Express' });
+});
+router.get('/negocio', function(req, res, next) {
+  res.render('negocio', { title: 'Express' });
+});
+router.get('/pedidos', function(req, res, next) {
+  res.render('pedidos', { title: 'Express' });
+});
+router.get('/sobre', function(req, res, next) {
+  res.render('sobre', { title: 'Express' });
+});
+router.get('/somos', function(req, res, next) {
+  res.render('somos', { title: 'Express' });
+});
 module.exports = router;
